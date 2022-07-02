791a4f55222d91b293e4bfb031b047fb568309e0
kwhong95 <kwhong95@kakao.com>
2022-07-02 21:58:36 +0900

    Implement Global Style with Emtoion

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Test from "Test";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { reset } from 'styles'
+import { Global } from '@emotion/react'
+import Test from 'Test'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
+    <Global styles={reset} />
     <Test />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
